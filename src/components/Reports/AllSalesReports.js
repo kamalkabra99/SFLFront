f62bd5b7ece4ad07556e648f0057dde0d8931b29
LokeshWebment <lokesh@webment.com>
2025-01-22 10:56:53 +0530

    commit

--- src/components/Reports/AllSalesReports.js
+++ src/components/Reports/AllSalesReports.js
@@ -84,13 +84,7 @@ class AllSalesReport extends Component {
         
       ],
       yearList:[
-        {value:2025,label:"2025"},
-        {value:2024,label:"2024"},
-        {value:2023,label:"2023"},
-        {value:2022,label:"2022"},
-        {value:2021,label:"2021"},
-        {value:2020,label:"2020"},
-        {value:2019,label:"2019"},        
+        {value:2025,label:"2025"},   
       ],
       Month:"",
       monthErr:false,
